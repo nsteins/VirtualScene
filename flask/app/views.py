a34fe27c13e527ab7f32c034b7a40d1fd65fa985
Nat Steinsultz <Nat.Steinsultz@gmail.com>
2016-03-19 16:15:39 -0400

    Added ability to search for albums or artist name using pgsql text search. Also added support for utf-8 album/artist names and an error page for albums without recommendations.

--- flask/app/views.py
+++ flask/app/views.py
@@ -17,44 +17,57 @@ def connectToDB():
 
 @app.route('/',methods=['GET','POST'])
 @app.route('/index',methods=['GET','POST'])
-
 def index():
+    form = SearchForm()
+    return render_template('index.html',form=form)
+
+
+@app.route('/search',methods=['GET','POST'])
+def search():
     conn = connectToDB()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    try:
-        cur.execute("SELECT * FROM albums WHERE name=%s",('Whenever, If Ever',))
-        album = cur.fetchone()
-    except:
-        album = {}
-        print('Error Selecting Album')
+    search_query = """SELECT * FROM albums WHERE (tsv @@ to_tsquery(%s))
+                      ORDER BY ts_rank(tsv,to_tsquery(%s))"""
 
-    query = """SELECT name,artist,url,id,fan_size,COUNT(*) FROM albums
-     JOIN relationship ON albums.id = relationship.album_id
-     WHERE relationship.fan_id IN (SELECT fan_id FROM relationship
-     WHERE album_id IN (SELECT id FROM albums WHERE name=%s))
-     GROUP BY name,artist,url,id,fan_size
-     ORDER BY COUNT(*) DESC"""
-
-    try:
-        cur.execute(query,('Whenever, If Ever',))
-        album_recs = cur.fetchall()
-    except:
-        print('Error Fetching Recommendations')
-
+    search_results = {};
     form = SearchForm()
     if form.validate_on_submit():
         print form.name.data
         try:
-            cur.execute("SELECT * FROM albums WHERE name=%s",(form.name.data,))
-            album = cur.fetchone()
+            ts_str = ' & '.join(form.name.data.split())
+            cur.execute(search_query,(ts_str,ts_str))
+            search_results = cur.fetchall()
         except:
             album = {}
-            print('Error Selecting Album')
-        try:
-            cur.execute(query,(form.name.data,))
-            album_recs = cur.fetchall()
-        except:
-            print('Error Fetching Recommendations')
- 
-    return render_template('index.html',album=album,album_recs=album_recs[1:10],
-        form=form)
+            print('Error Searching Album')
+
+    return render_template('search.html',search_results = search_results,form=form)
+
+@app.route('/rec/<album_id>',methods=['GET'])
+def rec(album_id):
+
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = """SELECT name,artist,url,id,fan_size,COUNT(*) FROM albums
+     JOIN relationship ON albums.id = relationship.album_id
+     WHERE relationship.fan_id IN (SELECT fan_id FROM relationship
+     WHERE album_id = %s)
+     GROUP BY name,artist,url,id,fan_size
+     ORDER BY COUNT(*) DESC"""
+
+    album_recs = {}
+
+    try:
+        cur.execute(query,(album_id,))
+        album_recs = cur.fetchall()
+    except:
+        print('Error fetching rec')
+    
+    form = SearchForm()
+
+    if album_recs == []:
+        return (render_template('error.html',form=form))
+    if (len(album_recs) > 10) :
+        return render_template('rec.html',album = album_recs[1],album_recs=album_recs[2:10],form=form)
+    else:
+        return render_template('rec.html',album = album_recs[1],album_recs=album_recs[2:],form=form)
--- flask/run.py
+++ flask/run.py
@@ -1,2 +1,7 @@
 from app import app
 app.run(debug=True)
+
+import sys
+if sys.version_info.major < 3:
+    reload(sys)
+sys.setdefaultencoding('utf8')
